87d87ef28ccbe80330b9fb863571fb86e312937a
varun-martha <varunkumar.martha@everest.engineering>
2026-06-08 23:50:28 +0530

    [Spec Kit] Update linter configuration ignores

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ const expoConfig = require("eslint-config-expo/flat");
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ["dist/*"],
+    ignores: ["dist/*", "build/*", "coverage/*", "*.min.js", "node_modules/*"],
     languageOptions: {
       parserOptions: {
         project: true,
