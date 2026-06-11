4f21e5cc24f24da1778351eeac2aef012c5647a6
varun-martha <varunkumar.martha@everest.engineering>
2026-06-11 12:55:53 +0530

    test: ignore server folder in root jest config

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,9 @@ module.exports = {
   testEnvironment: "node",
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)"
+  ],
+  testPathIgnorePatterns: [
+    "<rootDir>/node_modules/",
+    "<rootDir>/server/"
   ]
 };
